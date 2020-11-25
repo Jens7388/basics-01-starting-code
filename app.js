4f03bcf7dce7b5d25776c577766fc0139715c612
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-11-25 11:16:49 +0100

    Added link with v-bind

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course and learn Vue'
+            courseGoal: 'Finish the course and learn Vue',
+            vueLink: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
         };
     }
 });
